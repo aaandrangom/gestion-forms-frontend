ca2b81b6dd404a6ff1eab5a2330dc463b030fe67
Anthony Andrango <96062307+aaandrangom@users.noreply.github.com>
2024-03-11 17:07:08 -0500

    Mejoras del software

--- src/app/services/api/documento/documento.service.ts
+++ src/app/services/api/documento/documento.service.ts
@@ -12,7 +12,7 @@ import { API_CONFIG } from '../../../config/api.config';
   providedIn: 'root',
 })
 export class Formulario1Service {
-  private apiUrl: string = `${API_CONFIG.development}/documento`;
+  private apiUrl: string = `${API_CONFIG.production}/documento`;
 
   constructor(private http: HttpClient) {}
 
